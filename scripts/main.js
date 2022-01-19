da49e812a17550dbfdc44a80c5f3a027fb4b7eed
David Yarbrough <yarbroughdavidc@gmail.com>
2022-01-18 19:51:06 -0600

    adding main.js for the purposes of the Dev Tools demo

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+console.log("Main has started execution");
+
+const mainString = "This String is the Main Data";
+
+const mainArray = ["thing 1", "thing 2"];
+
+console.log("Main has finished execution");
